8fa717e1044f502ef0bc4c40eddbe994ac564678
floresenhalloween <42480199+fbientrigo@users.noreply.github.com>
2022-03-31 20:36:13 -0300

    experimentacion

--- CommitSpace/experiment.py
+++ CommitSpace/experiment.py
@@ -6,7 +6,12 @@ f = open('save1')
 # returns JSON object as
 # a dictionary
 data = json.load(f)
- 
+
+bosslist = []
+
+for obj in data:
+    bosslist.append
+
 # list
 for i in data.saves:
     print(i)
